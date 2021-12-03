b490bf45a2c8aa2e89e61798f4819d1f34c6f0f5
Jinbong <darkbrow@gmail.com>
2021-12-04 07:34:52 +0900

    update

--- src/Say.popclipext/say.sh
+++
@@ -1,6 +0,0 @@
-IS_KOR=$(echo "$POPCLIP_TEXT" | head -c 100 | iconv -f UTF-8-MAC -t UTF-8 | perl -Mutf8 -CI -ne 'print if /[ㄱ-힣]/')
-if [ ${#IS_KOR} -gt 0 ]; then
-    say -v Yuna "$POPCLIP_TEXT"
-else
-    say -v Samantha "$POPCLIP_TEXT"
-fi
